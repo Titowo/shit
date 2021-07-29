2cc433877b68ab384fe2257f9bea27812844ec35
Titowo <pedrotitojerial@gmail.com>
2021-07-29 16:02:30 -0400

    :)

--- Folder/#3/wash_hands.py
+++ Folder/#3/wash_hands.py
@@ -1,2 +1,7 @@
 def wash_hands(N, nM):
-	
+	tdays = nM*30
+	tsecs = N*21*tdays
+	tmins = tsecs/60
+	dmins = tmins - int(tmins)
+	secs = dmins*60
+	return str(int(tmins))+" minutes and "+ str(int(secs)) +" seconds"
